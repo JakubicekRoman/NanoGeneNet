ff13141e4522bc4d598363cb9dedf3bf4b7c1f53
Jakubíček Roman (119710) <jakubicek@vutbr.cz>
2021-11-06 18:36:51 +0100

    pridane mereni casu...
    Signed-off-by: Jakubíček Roman (119710) <jakubicek@vutbr.cz>

--- validace_det.py
+++ validace_det.py
@@ -17,6 +17,8 @@ import torch
 import random
 import utilities
 import pandas as pd
+import pickle
+import time
 
 import h5py
 import loaders2
@@ -97,142 +99,125 @@ class_dice=np.array((0,))
 res_table = pd.DataFrame(data=[], columns=['FileName', 'ID_signal', 'Gene', 'Dice', 'Gene position anot orig', 'Gene position anot sub', 'Gene position predicted orig',  'Gene position predicted sub', 'ID_image'] )                                         
 
 test_list = test_list_o
-# test_list = np.random.permutation( test_list_o )[0:len( test_list_o ):1]
+test_list = np.random.permutation( test_list_o )[0:len( test_list_o ):10]
 # test_list = np.random.permutation( test_list_o )[0:20:1]
 
 batch = 1
 
-hf = h5py.File('D:\jakubicek\Bioinformatika\Models\Export_Images\pst_signals.h5', 'w')
+T = []
+N = []
+
+# hf = h5py.File('D:\jakubicek\Bioinformatika\Models\Export_Images\pst_signals.h5', 'w')
+# hf = h5py.File('D:\jakubicek\Bioinformatika\Models\Export_Images\info.h5', 'w')
 
 for i in range(0, len(test_list), batch):
 # for ite in range(0, 10, batch):
     with torch.no_grad():
         
+        
         net.train(mode=False)            
         # sample, lbl, clbl = loaders2.Load_cut_signal_h5(i, batch, test_list, dictGen)
-        sample, lbl, clbl = loaders2.Load_whole_signal_h5(test_list[i], dictGen)
+        sample, lbl, clbl = loaders2.Load_whole_signal_h5(test_list[i], dictGen)      
+        
+        t = time.time()
+        
+        N.append(sample.shape[1])
         
         net.init_hiden(batch)            
         pred = net(sample.cuda())
         pred = F.softmax(pred, dim=2)
 
-        lbl = lbl.permute([0,2,1]).cuda()
-        lbl = F.interpolate(lbl, ( pred.shape[1]))
-        lbl = lbl[:,0,:]
-        pred = pred.permute([0,2,1])
-        # lbl = lbl.squeeze()
+        elapsed = time.time() - t
+        T.append(elapsed)
         
-        GT = lbl.detach().cpu().numpy()
-        P = pred[:,1,:].detach().cpu().numpy()>0.5
+        # lbl = lbl.permute([0,2,1]).cuda()
+        # lbl = F.interpolate(lbl, ( pred.shape[1]))
+        # lbl = lbl[:,0,:]
+        # pred = pred.permute([0,2,1])
+        # # lbl = lbl.squeeze()
         
-        dice = torch.tensor(np.zeros((1), dtype=np.float32))
-        dice[0] = utilities.dice_torch(torch.tensor(P), torch.tensor(GT))
-        test_dice.append(dice.numpy()[0])
+        # GT = lbl.detach().cpu().numpy()
+        # P = pred[:,1,:].detach().cpu().numpy()>0.5
         
-        # test_acc.append( np.mean( np.sum( np.sum( GT==P,2),1) / (GT.shape[1]*GT.shape[2]) )  )
-        # class_acc = np.concatenate((class_acc, ( np.sum( np.sum( GT==P,2),1) / (GT.shape[1]*GT.shape[2]) ) ) )
+        # dice = torch.tensor(np.zeros((1), dtype=np.float32))
+        # dice[0] = utilities.dice_torch(torch.tensor(P), torch.tensor(GT))
+        # test_dice.append(dice.numpy()[0])
+        
+        # # test_acc.append( np.mean( np.sum( np.sum( GT==P,2),1) / (GT.shape[1]*GT.shape[2]) )  )
+        # # class_acc = np.concatenate((class_acc, ( np.sum( np.sum( GT==P,2),1) / (GT.shape[1]*GT.shape[2]) ) ) )
+        # # class_lbl = np.concatenate( (class_lbl, clbl.numpy() ) )
+        # # class_dice = np.concatenate( (class_dice, dice.numpy() ) )
+        
+        # plt.figure
+        # plt.plot(lbl.detach().cpu().numpy()[0,:])
+        # plt.plot(pred.detach().cpu().numpy()[0,1,:])
+        # plt.ylim([0.0,1])
+        # num = '0000000' + str(i)
+        # num = num[-6:]
+        # plt.savefig('D:\\jakubicek\\Bioinformatika\\Models\\Export_Images\\' + 'Image_' + num + '.png' )
+        # plt.show()
+        # plt.close()
+         
+        # a = test_list[i]['tname'].split('\\')[-1]
+        # FileName = test_list[i]['file_path'].split('\\')[-1]
+        # f = h5py.File(test_list[i]['file_path'],'r')
+        # loc = np.asarray(f[a]['coord']).astype(np.float32)
+        # loc.sort() 
+        
+        # ind = [ii for ii, x in enumerate(np.squeeze(P)) if x ]    
+        # if not ind:
+        #     loc_pred_sub = [0,0]
+        # else:
+        #     loc_pred_sub = [ind[0], ind[-1]]
+        
+        # res_table.loc[(i,'FileName')] =   FileName 
+        # res_table.loc[(i,'ID_signal')] =  a 
+        # res_table.loc[(i,'Gene')] = clbl.detach().cpu().numpy()
+        # res_table.loc[(i,'Dice')] = dice[0].detach().cpu().numpy()
+        # res_table.loc[(i,'Gene position anot orig')] = loc
+        # res_table.loc[(i,'Gene position anot sub')] = (loc/16).astype( np.int64 ).tolist()
+        # res_table.loc[(i,'Gene position predicted orig')] = ( (np.array( loc_pred_sub)*16).astype( np.int64 ) ).tolist()
+        # res_table.loc[(i,'Gene position predicted sub')] = loc_pred_sub
+        # res_table.loc[(i,'ID_image')] = num
+          
+        # torch.cuda.empty_cache()
+    
         # class_lbl = np.concatenate( (class_lbl, clbl.numpy() ) )
         # class_dice = np.concatenate( (class_dice, dice.numpy() ) )
         
-        plt.figure
-        plt.plot(lbl.detach().cpu().numpy()[0,:])
-        plt.plot(pred.detach().cpu().numpy()[0,1,:])
-        plt.ylim([0.0,1])
-        num = '0000000' + str(i)
-        num = num[-6:]
-        plt.savefig('D:\\jakubicek\\Bioinformatika\\Models\\Export_Images\\' + 'Image_' + num + '.png' )
-        plt.show()
-        plt.close()
-         
-        a = test_list[i]['tname'].split('\\')[-1]
-        FileName = test_list[i]['file_path'].split('\\')[-1]
-        f = h5py.File(test_list[i]['file_path'],'r')
-        loc = np.asarray(f[a]['coord']).astype(np.float32)
-        loc.sort() 
-        
-        ind = [ii for ii, x in enumerate(np.squeeze(P)) if x ]    
-        if not ind:
-            loc_pred_sub = [0,0]
-        else:
-            loc_pred_sub = [ind[0], ind[-1]]
-        
-        res_table.loc[(i,'FileName')] =   FileName 
-        res_table.loc[(i,'ID_signal')] =  a 
-        res_table.loc[(i,'Gene')] = clbl.detach().cpu().numpy()
-        res_table.loc[(i,'Dice')] = dice[0].detach().cpu().numpy()
-        res_table.loc[(i,'Gene position anot orig')] = loc
-        res_table.loc[(i,'Gene position anot sub')] = (loc/16).astype( np.int64 ).tolist()
-        res_table.loc[(i,'Gene position predicted orig')] = ( (np.array( loc_pred_sub)*16).astype( np.int64 ) ).tolist()
-        res_table.loc[(i,'Gene position predicted sub')] = loc_pred_sub
-        res_table.loc[(i,'ID_image')] = num
-          
-        torch.cuda.empty_cache()
-    
-        class_lbl = np.concatenate( (class_lbl, clbl.numpy() ) )
-        class_dice = np.concatenate( (class_dice, dice.numpy() ) )
-        
         print(str(i/len(test_list)*100))
         
-        hf.create_dataset(num, data=pred[:,1,:].detach().cpu().numpy())
+        # hf.create_dataset(num, data=pred[:,1,:].detach().cpu().numpy())
         
-hf.close()
+# hf.close()
 
-utilities.save_to_excel(res_table, 'D:\\jakubicek\\Bioinformatika\\Models\\Export_Images' , 'Results' + num + '.xlsx')
+# utilities.save_to_excel(res_table, 'D:\\jakubicek\\Bioinformatika\\Models\\Export_Images' , 'Results' + num + '.xlsx')
     
-hd = utilities.comp_class_acc(class_lbl, class_dice)       
-plt.figure
-plt.bar(np.arange(0,8), hd)
-# plt.ylim([0.5, 1.0])
-plt.show()     
+# hd = utilities.comp_class_acc(class_lbl, class_dice)       
+# plt.figure
+# plt.bar(np.arange(0,8), hd)
+# # plt.ylim([0.5, 1.0])
+# plt.show()     
     
     
     
+## # time demanding 
+# hf = h5py.File('D:\jakubicek\Bioinformatika\Models\Export_Images\info.h5', 'r')
+# N = np.asarray(hf['lenghts']).astype(np.float32)
+# times = np.asarray(hf['times']).astype(np.float32)
 
-    # n=0   
-    # for m in range(0, len(test_list), 1):
-           
-    #     net.init_hiden(batch)
-    #     net.train(mode=False)
-    #     # net.zero_grad()
-        
-    #     sample,lbl = loaderWin(m, test_list, batch )
-        
-    #     pred = net(sample.cuda())
-    #     net.zero_grad()
-          
-    #     pred = F.softmax(pred, dim=2) 
-    
-    #     lbl = lbl.permute([0,2,1]).cuda()
-    #     lbl = F.interpolate(lbl, ( pred.shape[1]))
-    #     lbl = lbl[:,0,:]
-    
-    #     pred = pred.permute([0,2,1])
-        
-    #     # loss = nn.CrossEntropyLoss(weight=torch.tensor((0.1, 0.9)).cuda() )( pred,  lbl.type(torch.long) )
-    
-    #     GT = lbl.detach().cpu().numpy()
-    #     P = pred[:,1,:].detach().cpu().numpy()>0.5
-    #     test_acc.append( np.mean( np.sum( GT==P , 1) / GT.shape[1] ) )
-    
-    #     torch.cuda.empty_cache()
-        
-    #     if n%100 == 0:
-            
-    #         plt.figure
-    #         plt.plot(test_acc)
-    #         plt.ylim([0.0,1])
-    #         plt.show()
-            
-    #         # plt.figure
-    #         # plt.plot(lbl.detach().cpu().numpy()[0,:])
-    #         # plt.plot(pred.detach().cpu().numpy()[0,1,:])
-    #         # # plt.plot(P[0,:])
-    #         # # plt.ylim([0.7,1])
-    #         # plt.show()        
-            
-    #     n=n+1
-        
-    # torch.cuda.empty_cache()
+# ind = np.argsort(N)
+
+# N = N[ind]
+# times = times[ind]
+
+
+# plt.figure()
+# plt.plot(N,times)
+
+
+
+
 
 
 
